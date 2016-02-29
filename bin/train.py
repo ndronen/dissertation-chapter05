f76d94d7bd6c09d074c603b9b5e4a4a16db08216
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-02-29 10:03:57 -0700

    Snapshot of training scripts

--- bin/train.py
+++ bin/train.py
@@ -20,20 +20,18 @@ import modeling.parser
 
 def main(args):
     model_id = build_model_id(args)
+
     model_path = build_model_path(args, model_id)
+
     setup_model_dir(args, model_path)
 
     json_cfg = load_model_json(args, x_train=None, n_classes=None)
     config = ModelConfig(**json_cfg)
-    if args.verbose:
-    p    print("config " + str(config))
 
     sys.path.append(args.model_dir)
     import model
     from model import fit
 
-    if args.verbose:
-        print("fitting model")
     model.fit(config)
 
 if __name__ == '__main__':
